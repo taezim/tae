58af9178c96423663a4c84a50670f1daf9b7eea9
taezim <156161065+taezim@users.noreply.github.com>
2024-04-23 16:20:03 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 11.91 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        int a = overwrite_string.length();
+        int b = my_string.length();
+        answer = my_string.substring(0,s)+overwrite_string+my_string.substring(s+a,b);
+        return answer;
+    }
+}
