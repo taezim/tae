da174868fd29ca9eff64236f848cb0d19041f5a9
taezim <156161065+taezim@users.noreply.github.com>
2024-05-24 16:28:13 +0900

    [level 0] Title: 나이 출력, Time: 7.56 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 2022-age+1;
+        System.out.println("2022년도 기준"+age+"살 이므로"+answer+"년생입니다.");
+        return answer;
+    }
+}
