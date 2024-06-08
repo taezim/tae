b0e29839b4459b308852f0eb84004d79e97034c3
taezim <156161065+taezim@users.noreply.github.com>
2024-06-08 13:09:45 +0900

    [level 0] Title: 5명씩, Time: 0.01 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String[] solution(String[] names) {
+        String[] answer = {};
+        int idx = 0;
+        
+        if(names.length%5==0){
+            answer = new String[names.length/5];
+        }
+        else{
+            answer = new String[names.length/5+1];
+        }
+        
+        for(int i=0; i<names.length; i+=5){
+            answer[idx] = names[i];
+            idx++;
+        }
+        return answer;
+    }
+}
